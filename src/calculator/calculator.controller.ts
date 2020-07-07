c420faa904238801d99f2747513979aed3b3ba23
WhatTheFar <jakpat.m@gmail.com>
2020-07-07 16:59:25 +0700

    feat(swagger): init swagger

--- src/calculator/calculator.controller.ts
+++ src/calculator/calculator.controller.ts
@@ -1,16 +1,21 @@
 import { Controller, Get, ParseIntPipe, Param } from '@nestjs/common';
 import { CalculatorService } from './calculator.service';
+import { ApiOkResponse, ApiParam } from '@nestjs/swagger';
 
 @Controller('calculator')
 export class CalculatorController {
   constructor(private readonly calculatorService: CalculatorService) {}
 
   @Get('fibo/:order')
+  @ApiParam({ name: 'order', type: Number, example: 1 })
+  @ApiOkResponse({ type: Number })
   getFibonacci(@Param('order', ParseIntPipe) order: number): Promise<number> {
     return this.calculatorService.fibonacci(order);
   }
 
   @Get('fibo/faster/:order')
+  @ApiParam({ name: 'order', type: Number, example: 1 })
+  @ApiOkResponse({ type: Number })
   getFibonacciFaster(
     @Param('order', ParseIntPipe) order: number,
   ): Promise<number> {
@@ -18,6 +23,8 @@ export class CalculatorController {
   }
 
   @Get('fibo/fastest/:order')
+  @ApiParam({ name: 'order', type: Number, example: 1 })
+  @ApiOkResponse({ type: Number })
   getFibonacciFastest(
     @Param('order', ParseIntPipe) order: number,
   ): Promise<number> {

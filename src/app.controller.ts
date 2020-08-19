cb37c31e74d8733ac450d0616feb50e11a18b6d0
WhatTheFar <jakpat.m@gmail.com>
2020-08-19 15:58:12 +0700

    feat: v2

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,4 +9,9 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('v2')
+  getHelloV2(): string {
+    return this.appService.getHello();
+  }
 }
